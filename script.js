b134c75340881e41b137b2c31a6f95aa7527b181
Your Name <rohit.savalagi@netcracker.com>
2023-02-10 10:52:46 +0530

    Comments Removed

--- script.js
+++ script.js
@@ -20,23 +20,3 @@ const John = {
 
 console.log(Mark.calcBMI() < John.calcBMI() ? `John's BMI (${John.BMI}) is higher than Mark's (${Mark.BMI})`: `Mark's BMI (${Mark.BMI}) is higher than John's (${John.BMI}).`);
 
-
-
-// const Mark = {
-//     fullName: "Mark Miller",
-//     mass: 78,
-//     height: 1.69,
-// }
-
-// const John = {
-//     fullName: "John Smith",
-//     mass: 92,
-//     height: 1.95,
-// }
-
-// calcBMI = function(){
-//     this.BMI = this.mass / (this.height * this.height);
-//     return this.BMI;
-// }
-
-// console.log(calcBMI.call(Mark) < calcBMI.call(John) ? `John's BMI (${John.BMI}) is higher than Mark's (${Mark.BMI})`: `Mark's BMI (${Mark.BMI}) is higher than John's (${John.BMI}).`);
